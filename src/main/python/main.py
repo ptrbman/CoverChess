5133718db64163261d4f51eb5adc7f2833bfc630
Peter Backeman <peter@backeman.se>
2020-12-04 22:45:21 +0100

    Add drag'n'drop

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,76 +1,16 @@
-
-##
-## Either fix barracks or remove it
-## Make sure the reset works
-##
-## Next step: separate to server/client even here
-##
-
 from fbs_runtime.application_context.PyQt5 import ApplicationContext, cached_property
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout, QMainWindow, QMessageBox, QAction, qApp, QDialogButtonBox, QLineEdit, QSizePolicy, QLabel
-from PyQt5.QtGui import QIcon, QFont, QDrag
+from PyQt5.QtGui import QIcon, QFont, QDrag, QPixmap, QPainter
 from PyQt5.QtCore import pyqtSlot, QMimeData
 from PyQt5.QtCore import Qt
 from PyQt5 import QtSvg
 
 from player import *
 from board import Board
+from squares import BoardSquare, BarracksSquare
+from game import Game
 
 import sys
-
-class BoardSquare(QtSvg.QSvgWidget):
-    WHITE_SQUARE = 0
-    BLACK_SQUARE = 1
-    GREEN_SQUARE = 2
-    RED_SQUARE = 3
-    WHITE_ROOK = 4
-    BLACK_ROOK = 5
-    PIECES = 6
-
-    def __init__(self, images, background, click):
-        super(QWidget, self).__init__(images[background])
-        self.images = images
-        self.mouseReleaseEvent=click
-
-        self.background = background
-        self.piece = background
-
-        self.label = QLabel(self)
-        self.label.setText("   ")
-        self.label.move(10, 10)
-        self.label.setFont(QFont('Arial', 48))
-
-
-    def reset(self):
-        self.piece = self.background
-        self.load(self.images[self.background])
-        self.label.setText("")
-
-    def setPiece(self, piece):
-        self.piece = piece
-        if piece > 1:
-            self.label.setText("")
-        self.load(self.images[piece])
-
-    def hasHeightForWidth(self):
-        return True
-
-    def heightForWidth(self, w):
-        return w
-
-    def setScore(self, score):
-        if self.piece < 4:
-            if (score < 0):
-                p = BoardSquare.RED_SQUARE
-            elif (score > 0):
-                p = BoardSquare.GREEN_SQUARE
-            else:
-                p = self.background
-
-            self.piece = p
-            self.load(self.images[p])
-            self.label.setText(str(score))
-            self.adjustSize()
 
 class MainWindow(QMainWindow):
     def __init__(self, ctx):
@@ -99,39 +39,34 @@ class ChangeBoardDialog(QDialog):
         self.setLayout(self.layout)
 
 
+##
+## Main Application
+##
+
 class AppContext(ApplicationContext):
     @cached_property
     def main_window(self):
         return MainWindow(self)
 
-    def initBoard(self):
-        # We have to destroy old board if existing
-        for bs in self.squares:
-            for b in bs:
-                b.deleteLater()
-
-        self.board = Board(self.rows, self.cols)
-        self.squares = []
-
-        for r in range(self.rows):
-            self.squares.append([BoardSquare(self.piece_set, 0, self.gen_clickhandler(r, c)) for c in range(self.cols)])
-
-    def __init__(self, rows, cols, max_turns):
+    def __init__(self):
         super().__init__()
         self.main_window.title = 'CoverChess'
-        self.left = 10
-        self.top = 10
+        self.left = 0
+        self.top = 0
         self.width = 800
         self.height = 800
 
-        self.rows = rows
-        self.cols = cols
-        self.max_turns = max_turns*2
-        self.squares = []
-        self.turns = 0
-        self.next_white = True
-        self.player_white = True
+        # Default values
+        self.rows = 4
+        self.columns = 4
+        self.max_moves = 2*2
+        self.white_human = False
+        self.black_human = True
 
+        self.squares = []
+        self.barracks = []
+
+        # Images for the pieces
         self.piece_set = [0] * BoardSquare.PIECES
         self.piece_set[BoardSquare.WHITE_SQUARE] = self.get_resource('images/solid_white.svg')
         self.piece_set[BoardSquare.BLACK_SQUARE] = self.get_resource('images/solid_black.svg')
@@ -140,85 +75,8 @@ class AppContext(ApplicationContext):
         self.piece_set[BoardSquare.WHITE_ROOK] = self.get_resource('images/white_rook.svg')
         self.piece_set[BoardSquare.BLACK_ROOK] = self.get_resource('images/black_rook.svg')
 
-        self.initBoard()
         self.initUI()
-        self.initBoardLayout()
-
-
-    def do_move(self, row, col):
-        self.turns += 1
-        if self.next_white:
-            self.next_white = False
-            self.board.set_cell(row, col, 1)
-            self.squares[row][col].setPiece(BoardSquare.WHITE_ROOK)
-        else:
-            self.next_white = True
-            self.board.set_cell(row, col, -1)
-            self.squares[row][col].setPiece(BoardSquare.BLACK_ROOK)
-
-        self.updateScores()
-        self.grid.update()
-
-        if self.turns == self.max_turns:
-            self.game_over()
-
-        if (self.player_white and not self.next_white) or (not self.player_white and self.next_white):
-            self.minmax_move()
-
-
-    def handle_click(self, row, col):
-        if (self.board.get_cell(row, col) == 0):
-            self.do_move(row, col)
-
-    def minmax_move(self):
-        (row, col) = minmax(self.board, self.player_white, 2)
-        self.do_move(row, col)
-
-    def random_move(self):
-        moves = []
-        for r in range(self.rows):
-            for c in range(self.cols):
-                if self.board.get_cell(r, c) == 0:
-                    moves.append((r, c))
-        (r, c) = random.choice(moves)
-        self.do_move(r, c)
-
-    def game_over(self):
-        score = self.board.get_final_score()
-        msgBox = QMessageBox()
-        msgBox.setIcon(QMessageBox.Information)
-        if score > 0:
-            title = "White wins!"
-        elif score < 0:
-            title = "Black wins!"
-        else:
-            title = "Tie!"
-
-        msgBox.setText(title + "\nFinal score: " + str(score))
-        msgBox.setWindowTitle(title)
-        msgBox.exec()
-        self.reset()
-
-    def reset(self):
-        self.board.reset()
-        for r in range(self.rows):
-            for c in range(self.cols):
-                self.squares[r][c].reset()
-        self.updateScores()
-        self.grid.update()
-        self.turns = 0
-        self.next_white = True
-
-
-    def updateScores(self):
-        for r in range(self.rows):
-            for c in range(self.cols):
-                score = self.board.get_score(r, c)
-                self.squares[r][c].setScore(score)
-
-
-    def gen_clickhandler(self, row, col):
-        return lambda _ : self.handle_click(row, col)
+        self.new_game()
 
     def initBoardLayout(self):
         self.wid = QWidget()
@@ -226,19 +84,22 @@ class AppContext(ApplicationContext):
         self.grid = boardLayout
 
         for r in range(self.rows):
-            for c in range(self.cols):
+            for c in range(self.columns):
                 boardLayout.addWidget(self.squares[r][c], r,c)
 
-        # barracksLayout = QGridLayout()
-        # for c in range(self.cols):
-        #     widget = QtSvg.QSvgWidget(self.get_resource('images/white_rook.svg'))
-        #     widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        #     barracksLayout.addWidget(widget, 0, c)
+        self.barracks = []
+        barracksLayout = QGridLayout()
+        for c in range(self.columns):
+            widget = QtSvg.QSvgWidget(self.get_resource('images/white_rook.svg'))
+            widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            bsq = BarracksSquare(self.piece_set, BoardSquare.WHITE_ROOK, self.gendrag(c))
+            self.barracks.append(bsq)
+            barracksLayout.addWidget(bsq, 0, c)
 
         layout = QVBoxLayout()
 
         layout.addLayout(boardLayout)
-        # layout.addLayout(barracksLayout)
+        layout.addLayout(barracksLayout)
 
         self.wid.setLayout(layout)
         self.main_window.setCentralWidget(self.wid)
@@ -278,12 +139,112 @@ class AppContext(ApplicationContext):
         playerColorMenu.addAction(whiteAction)
         playerColorMenu.addAction(blackAction)
 
-        if self.player_white:
+        if self.white_human:
             whiteAction.setChecked(True)
-        else:
+        if self.black_human:
             blackAction.setChecked(True)
 
         settingsMenu.addAction(changeBoardAction)
+
+    def update_graphics(self):
+        for r in range(self.rows):
+            for c in range(self.columns):
+                (piece, score) = self.game.get_status(r, c)
+                if (piece == 1):
+                    piece = BoardSquare.WHITE_ROOK
+                if (piece == -1):
+                    piece = BoardSquare.BLACK_ROOK
+                self.squares[r][c].setStatus(piece, score)
+                # self.squares[r][c].setScore(self.game.get_score(r, c))
+
+        self.grid.update()
+
+
+
+    # Restart the game
+    # - Resize board and create squares
+    # - Reset game
+    # - Update graphics
+    def new_game(self):
+        # We have to destroy old board if existing
+        for bs in self.squares:
+            for b in bs:
+                b.deleteLater()
+
+        self.squares = []
+        for r in range(self.rows):
+            # self.squares.append([BoardSquare(self.piece_set, 0, self.gen_clickhandler(r, c)) for c in range(self.columns)])
+            self.squares.append([BoardSquare(self.piece_set, 0, self.gen_clickhandler(r, c)) for c in range(self.columns)])
+
+        self.game = Game(self.rows, self.columns, self.max_moves)
+        self.initBoardLayout()
+        self.update_graphics()
+
+
+    def do_move(self, row, col):
+        gameOver = self.game.make_move(row, col)
+        self.update_graphics()
+        if (gameOver):
+            self.game_over()
+
+        # Computer move?
+        self.check_computer_move()
+
+    def handle_click(self, row, col, src):
+        if (self.game.valid_move(row, col)):
+            self.barracks[src].used()
+            self.do_move(row, col)
+
+
+    # TODO: Move to AI file
+    def minmax_move(self):
+        (row, col) = minmax(self.game.board, self.game.next_player() == Game.BLACK, 2)
+        self.do_move(row, col)
+
+    # def random_move(self):
+    #     moves = []
+    #     for r in range(self.rows):
+    #         for c in range(self.columns):
+    #             if self.board.get_cell(r, c) == 0:
+    #                 moves.append((r, c))
+    #     (r, c) = random.choice(moves)
+    #     self.do_move(r, c)
+
+    def game_over(self):
+        winner = self.game.get_winner()
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        if winner == Game.WHITE:
+            title = "White wins!"
+        elif winner == Game.BLACK:
+            title = "Black wins!"
+        else:
+            title = "Tie!"
+
+        msgBox.setText(title)
+        msgBox.setWindowTitle(title)
+        msgBox.exec()
+        self.new_game()
+
+
+    def gen_clickhandler(self, row, col):
+        return lambda src : self.handle_click(row, col, src)
+
+    def gendrag(self, idx):
+        return lambda _ : self.drag_test(idx)
+
+    def drag_test(self, idx):
+        drag = QDrag(self.main_window)
+        mimeData = QMimeData()
+        mimeData.setText(str(idx))
+        drag.setMimeData(mimeData)
+        pixmap = QPixmap(150, 150)
+        painter = QPainter(pixmap)
+        painter.drawPixmap(self.barracks[idx].rect(), self.barracks[idx].grab())
+        painter.end()
+        drag.setPixmap(pixmap)
+        drag.exec_()
+
 
     def change_board_dialog(self):
         dlg = ChangeBoardDialog(self.main_window)
@@ -294,7 +255,7 @@ class AppContext(ApplicationContext):
             newWidth = dlg.width_input.text()
             if newHeight.isnumeric() and newWidth.isnumeric() and 0 < int(newHeight) and 0 < int(newWidth):
                 self.rows = int(newHeight)
-                self.cols = int(newWidth)
+                self.columns = int(newWidth)
                 self.initBoard()
                 self.initBoardLayout()
                 self.reset()
@@ -306,13 +267,24 @@ class AppContext(ApplicationContext):
                 msgBox.exec()
 
     def set_player_color(self, white):
-        self.player_white = white
-        self.whiteAction.setChecked(white)
-        self.blackAction.setChecked(not white)
+        if (white):
+            if (self.white_human):
+                self.white_human = False
+                self.whiteAction.setChecked(False)
+            else:
+                self.white_human = True
+                self.whiteAction.setChecked(True)
+        else:
+            if (self.black_human):
+                self.black_human = False
+                self.blackAction.setChecked(False)
+            else:
+                self.black_human = True
+                self.blackAction.setChecked(True)
         self.check_computer_move()
 
     def check_computer_move(self):
-        if self.player_white != self.next_white:
+        if (self.game.next_player() == Game.WHITE and not self.white_human) or (self.game.next_player() == Game.BLACK and not self.black_human):
             self.minmax_move()
 
     def run(self):
@@ -321,6 +293,6 @@ class AppContext(ApplicationContext):
         return self.app.exec_()
 
 if __name__ == '__main__':
-    appctxt = AppContext(4,4,3)
+    appctxt = AppContext()
     exit_code = appctxt.run()
     sys.exit(exit_code)
